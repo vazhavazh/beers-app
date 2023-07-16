b300ca06d143545644646c1dd19dae286ba2f964
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-07-16 17:36:24 +0300

    Update OneBeerPage.jsx

--- src/pages/OneBeerPage/OneBeerPage.jsx
+++ src/pages/OneBeerPage/OneBeerPage.jsx
@@ -4,7 +4,7 @@ import {
   BeerName,
   BeerPageContainer,
   BeerTagline,
-  Ingredient,
+  // Ingredient,
   RecipeContainer,
   RecipeInstructions,
   RecipeTitle,
@@ -40,13 +40,13 @@ export const OneBeerPage = () => {
 
             <h4>Ingredients:</h4>
             <ul>
-              {beer.ingredients.malt.map((malt, index) => (
+              {/* {beer.ingredients.malt.map((malt, index) => (
                 <Ingredient key={index}>{malt.name}</Ingredient>
               ))}
               {beer.ingredients.hops.map((hop, index) => (
                 <Ingredient key={index}>{hop.name}</Ingredient>
               ))}
-              <Ingredient>Yeast: {beer.ingredients.yeast}</Ingredient>
+              <Ingredient>Yeast: {beer.ingredients.yeast}</Ingredient> */}
             </ul>
           </RecipeContainer>
 
